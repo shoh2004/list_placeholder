d569ca9618405f35f8f648d951c556711e183f6f
shohrux <shohruxergashev67@gmail.com>
2022-07-15 21:41:16 +0500

    placeholder

--- bin/list_placeholder3.dart
+++ bin/list_placeholder3.dart
@@ -5,5 +5,20 @@ Argument int n
 Return the List
 
 */
+List func(int n){
+    int i = 0;
+    int y = 0;
+    List x = [];
+    while(n>i){
+    x.add(1);
+    i++;
 
-void main() {}
+    }
+    return x;
+}
+
+void main() {
+    print(func(6));
+}
+
+
